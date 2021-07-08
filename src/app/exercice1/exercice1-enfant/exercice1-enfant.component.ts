63f3f6a2d1c1d8d06fde1ba6a313cc4dbafb0105
Adrien <Adrien@DESKTOP-RC53543>
2021-07-08 14:58:14 +0200

    exercice 1

--- src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
+++ src/app/exercice1/exercice1-enfant/exercice1-enfant.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-exercice1-enfant',
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./exercice1-enfant.component.css']
 })
 export class Exercice1EnfantComponent implements OnInit {
-
+@Input() compteurenfant: number;
+@Output() changecompteur = new EventEmitter();
   constructor() { }
 
   ngOnInit() {
+  }
+
+  plus(){
+    this.compteurenfant ++;
+    this.changecompteur.emit({
+      value: this.compteurenfant
+    })
+  }
+  minus(){
+    this.compteurenfant--;
+    this.changecompteur.emit({
+      value: this.compteurenfant
+    })
   }
 
 }
